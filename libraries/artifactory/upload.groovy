5dea122d65839bcd7c379b705527ee1ad2479bcb
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2020-11-18 16:07:37 +0200

    Update upload.groovy

--- libraries/artifactory/upload.groovy
+++ libraries/artifactory/upload.groovy
@@ -2,4 +2,5 @@
 def call(def path) {
     println "Upload to ${path}"
     println "Upload source: ${pipelineConfig.artifacts_directory}"
+    println "Get it from keywords: ${artifacts_directory}"
 }
